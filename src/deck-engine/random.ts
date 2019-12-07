8a1fb9dae88b94e72dd367bfb393c7f4ed338db1
Dean Bowler <dean.bowler@gmail.com>
2019-12-07 17:41:00 +0000

    Reduce collisions for drawing random items with seed

--- src/deck-engine/random.ts
+++ src/deck-engine/random.ts
@@ -14,14 +14,35 @@ export const randomCard = () => anyDeck[randomIndex()];
 export const randomCards = (numberOfCards = 1) =>
   range(0, numberOfCards - 1).map(randomCard);
 
-export const drawRandomItemsWithSeed = <T>(seed: string, numberToDraw: number) => (
-  items: T[],
-) => {
+export const drawRandomItemsWithSeed = <T>(
+  seed: string,
+  numberToDraw: number,
+  avoidDuplicates = true,
+) => (items: T[]) => {
   const seeded = new Chance(seed);
+
+  const used: { [key: number]: number } = {};
+
+  const spread = Math.round(items.length / 10);
+  const getItemAvoidingDuplicates = (index: number) => {
+    let currIndex = used[index] ? used[index] + spread : index;
+    if (used[currIndex]) {
+      currIndex += spread;
+    }
+    if (currIndex >= items.length) currIndex = used[0] || 0;
+
+    used[index] = currIndex;
+
+    return items[currIndex];
+  };
+
+  const getItemMap = avoidDuplicates
+    ? getItemAvoidingDuplicates
+    : (index: number) => items[index];
 
   return range(0, numberToDraw)
     .map(() => seeded.integer({ min: 0, max: items.length - 1 }))
-    .map(i => items[i]);
+    .map(getItemMap);
 };
 
 export const drawItemsFromRandomPoint = <T>(
